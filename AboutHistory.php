b3346a31245f2f635864992dfada0ab815e50fcf
Tobie Allen <thestray259@gmail.com>
2023-08-07 08:22:40 -0600

    added rest of contact and about pages

---
+++ AboutHistory.php
@@ -0,0 +1,13 @@
+<?php
+include_once "MyHeader.php";
+?>
+
+<?php
+$MyHeader = "About/History Page";
+
+echo "<br> <br> History here.";
+?>
+
+<?php
+include_once "MyFooter.php";
+?>
---
+++ ContactEmail.php
@@ -0,0 +1,31 @@
+<?php
+include_once "MyHeader.php";
+?>
+
+<?php
+$MyHeader = "Contact/Email Page";
+
+echo "<br> Contact us using the form below! <br> <br>";
+?>
+
+<div class="container">
+    <form>
+        <label for="fname">First Name</label>
+        <input type="text" id="fname" name="firstname" placeholder="Your First Name..." />
+
+        <label for="lname">Last Name</label>
+        <input type="text" id="lname" name="lastname" placeholder="Your Last Name..." />
+
+        <label for="email">Email</label>
+        <input type="text" id="email" name="email" placeholder="Your Email..." />
+
+        <label for="subject">Subject</label>
+        <textarea type="text" id="subject" name="subject" placeholder="Write something..." style="height:200px"></textarea>
+
+        <input type="submit" value="Submit" />
+    </form>
+</div>
+
+<?php
+include_once "MyFooter.php";
+?>
