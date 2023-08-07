e213ffd4bfec7536ea5d986bf93fffe97b6dccae
Tobie Allen <thestray259@gmail.com>
2023-08-07 09:42:24 -0600

    mission statement

--- AboutMission.php
+++ AboutMission.php
@@ -5,7 +5,7 @@ include_once "MyHeader.php";
 <?php
 $MyHeader = "About/Mission Page";
 
-echo "<br> <br> [Team Name] mission statement here.";
+echo "<br> <br> Empowering Honkai Star Rail Enthusiasts: Our mission is to provide a dynamic and user-friendly platform that enables fans of the Honkai Star Rail universe to effortlessly track and manage their collection of characters through intuitive features and a visually engaging interface. We are committed to enhancing the gaming experience by offering a personalized tool that celebrates the passion for Honkai Star Rail.";
 ?>
 
 <?php
