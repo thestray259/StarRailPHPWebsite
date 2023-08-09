3fd1a5f839d1f28e267bd029fcce5ac8b176b208
Tobie Allen <thestray259@gmail.com>
2023-08-09 08:31:42 -0600

    made the subpages actual subpages

--- Menu.php
+++ Menu.php
@@ -7,11 +7,11 @@
         <ul>
             <li><a href="index.php">Home</a></li>
             <li><a href="AboutPage.php">About</a></li>
-            <li><a href="AboutMission.php">About</a></li>
-            <li><a href="AboutHistory.php">About</a></li>
+            <li><a href="About/AboutHistory.php">History</a></li>
+            <li><a href="About/AboutMission.php">Mission</a></li>
             <li><a href="ContactPage.php">Contact</a></li>
-            <li><a href="ContactEmail.php">Contact</a></li>
-            <li><a href="ContactLocations.php">Contact</a></li>
+            <li><a href="Contact/ContactEmail.php">Email</a></li>
+            <li><a href="Contact/ContactLocations.php">Locations</a></li>
         </ul>
     </nav>
 </header>
